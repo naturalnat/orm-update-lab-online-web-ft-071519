741b92f3e4156037fe2fe80b540f27448cd72213
Natalie Taktachev <ntaktach@gmail.com>
2019-08-20 00:23:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,19 +1,19 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :name, :grade, :id 
+  attr_accessor :name, :grade, :id
 
   def initialize(name, grade, id=nil),
     @name = name
     @grade = grade
-    @id = id 
+    @id = id
   end
 
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
-        name TEXT, 
+        name TEXT,
         grade TEXT
       )
     SQL
@@ -27,8 +27,8 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def save 
-    if self.id 
+  def save
+    if self.id
       self.update
     else
       sql = <<-SQL
@@ -41,5 +41,5 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
-  
+
 end
