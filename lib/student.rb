a42618a5358a19b1900a9382fe3f2d7777d1ff4d
Natalie Taktachev <ntaktach@gmail.com>
2019-08-20 00:33:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,18 +48,6 @@ class Student
   end
 
 
-  def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE name = ?
-      LIMIT 1
-    SQL
-
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
 
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
@@ -72,6 +60,19 @@ class Student
     name = row[1]
     grade = row[2]
     self.new(id, name, grade)
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
 end
