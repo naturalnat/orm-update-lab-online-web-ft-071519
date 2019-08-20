4b7d5e2a3c7d748aa48ce897478b3e5d354ee085
Natalie Taktachev <ntaktach@gmail.com>
2019-08-20 00:29:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,7 +67,7 @@ class Student
     end.first
   end
 
-  def update 
+  def update
     sql = "UPDATE students SET name = ?, grade = ?, WHERE id = ?"
 
     DB[:conn].execute(sql, self.name, self.grade, self.id)
