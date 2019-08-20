f850879a2a000b4435d26902345c90b6956292b1
Natalie Taktachev <ntaktach@gmail.com>
2019-08-20 00:35:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,10 +56,10 @@ class Student
   end
 
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)
+    id = row[2]
+    name = row[0]
+    grade = row[1]
+    self.new(name, grade, id)
   end
 
   def self.find_by_name(name)
