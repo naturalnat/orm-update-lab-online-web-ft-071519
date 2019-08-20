fcc71fc391a5f7aac9ed8b9e40c6c2b85ae6d87f
Natalie Taktachev <ntaktach@gmail.com>
2019-08-20 00:31:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,13 +47,7 @@ class Student
     student.save
   end
 
-  def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)
-  end
-
+  
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
@@ -72,4 +66,12 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end
+
 end
